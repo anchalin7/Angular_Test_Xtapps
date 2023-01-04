631b0de616de31fc7beab7018d4ab057ff7bc649
Anchalin Davis <49554445+anchalin7@users.noreply.github.com>
2023-01-04 18:14:01 +0530

    Angular Setup
    Typescript, JavaScript setup
    variable declarations
    interface
    class

---
+++ test_World/ts-hello/main.js
@@ -0,0 +1,12 @@
+function log(message) {
+    console.log(message);
+}
+var message = 'Hello World';
+log(message);
+function doSomthing() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally:' + i);
+}
+doSomthing();
---
+++ test_World/ts-hello/main.ts
@@ -0,0 +1,15 @@
+function log(message: string)
+{
+    console.log(message);
+}
+var message = 'Hello World';
+
+log(message);
+
+ function doSomthing(){
+    for(var i=0; i<5;i++){
+        console.log(i);
+    }
+    console.log('Finally:' + i);
+ }
+ doSomthing();
